c75daff3b645f5ea51517e2364c996599583ff69
Mario SAM <eu@mariosam.com.br>
2022-11-24 00:21:36 -0300

    Create SearchInsertPosition.go

---
+++ GO/SearchInsertPosition.go
@@ -0,0 +1,15 @@
+/**
+ * @version GO 1.19.3
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+func searchInsert(nums []int, target int) int {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] >= target {
+			return i
+		}
+	}
+	return len(nums)
+}
