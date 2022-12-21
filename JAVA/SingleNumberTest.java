a4b7e7386bdb0a505561a2dfe0c7de3c874232b7
Mario SAM <eu@mariosam.com.br>
2022-12-21 12:56:09 -0300

    Create SingleNumberTest.java

---
+++ JAVA/SingleNumberTest.java
@@ -0,0 +1,30 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SingleNumberTest {
+
+    @Test
+	public void testSingleNumber() throws Exception {
+        //Test 1
+        int want = 1;
+        int got = SingleNumber.singleNumber( new int[]{2,2,1} );
+        assertEquals(want, got);
+        //Test 2
+        want = 4;
+        got = SingleNumber.singleNumber( new int[]{4,1,2,1,2} );
+        assertEquals(want, got);
+        //Test 3
+        want = 1;
+        got = SingleNumber.singleNumber( new int[]{1} );
+        assertEquals(want, got);
+	}
+
+}
