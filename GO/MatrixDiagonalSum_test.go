f8addf632fd750cbe63b189c8619cf1f51971c0e
Mario SAM <eu@mariosam.com.br>
2023-01-27 00:48:20 -0300

    Create MatrixDiagonalSum_test.go

---
+++ GO/MatrixDiagonalSum_test.go
@@ -0,0 +1,31 @@
+/**
+ * @version GO 1.19.4
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ go test -timeout 999999s -run TestMatrixDiagonalSum
+ */
+package GO
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMatrixDiagonalSum(t *testing.T) {
+	tables := []struct {
+		want int
+		nums [][]int
+	}{
+		{25, [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}},
+		{8, [][]int{{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}}},
+		{5, [][]int{{5}}},
+	}
+
+	for _, table := range tables {
+		got := diagonalSum(table.nums)
+
+		if !reflect.DeepEqual(got, table.want) {
+			t.Errorf("Waiting for this %d but the return was this: %d", table.want, got)
+		}
+	}
+}
