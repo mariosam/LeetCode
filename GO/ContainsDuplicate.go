5e1bd0bbd7c8b23afcb93e7989f131103657d116
Mario SAM <eu@mariosam.com.br>
2023-02-04 01:15:05 -0300

    Create ContainsDuplicate.go

---
+++ GO/ContainsDuplicate.go
@@ -0,0 +1,22 @@
+/**
+ * @version GO 1.19.5
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+func containsDuplicate(nums []int) bool {
+	return len(nums) != len(unique(nums))
+}
+
+func unique(intSlice []int) []int {
+	keys := make(map[int]bool)
+	list := []int{}
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
