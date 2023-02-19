ee44875d1b71a0e1c788c3c1a51cd78a53f9d681
Mario SAM <eu@mariosam.com.br>
2023-02-19 00:14:47 -0300

    Create AddToArrayFormOfInteger.java

---
+++ JAVA/AddToArrayFormOfInteger.java
@@ -0,0 +1,21 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AddToArrayFormOfInteger {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", addToArrayForm( new int[]{1,2,3,0,0,0}, 33 ));
+    }
+
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+
+    }
+
+}
