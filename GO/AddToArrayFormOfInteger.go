d07314c8da0e50d5f3cce6ae7465a5ec33c13a89
Mario SAM <eu@mariosam.com.br>
2023-02-20 00:25:11 -0300

    Create AddToArrayFormOfInteger.go

---
+++ GO/AddToArrayFormOfInteger.go
@@ -0,0 +1,16 @@
+/**
+ * @version GO 1.20
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func addToArrayForm(num []int, k int) []int {
+
+}
