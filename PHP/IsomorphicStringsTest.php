c704886efb6c1e591e7fbd52ceb9252e3f935cae
Mario SAM <eu@mariosam.com.br>
2023-02-25 01:40:39 -0300

    Create IsomorphicStringsTest.php

---
+++ PHP/IsomorphicStringsTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ ./vendor/bin/phpunit IsomorphicStringsTest.php
+ */
+namespace PHP;
+
+use PHPUnit\Framework\TestCase;
+require ("IsomorphicStrings.php");
+
+class IsomorphicStringsTest extends TestCase {
+
+    public function testIsomorphicStrings() {
+        $obj = new IsomorphicStrings();
+        //Test 1
+        $want = true;
+        $got = $obj->isIsomorphic( "egg", "add" );
+        echo "\nTest 1: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+        //Test 2
+        $want = false;
+        $got = $obj->isIsomorphic( "foo", "bar" );
+        echo "\nTest 2: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+        //Test 3
+        $want = true;
+        $got = $obj->isIsomorphic( "paper", "title" );
+        echo "\nTest 3: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+    }
+}
