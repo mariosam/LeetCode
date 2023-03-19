28ee53b0276a6db1da69e2476d5689c14b5f4055
Mario SAM <eu@mariosam.com.br>
2023-03-19 00:03:46 -0300

    Create FindTheIndexOfTheFirstOccurrenceInString.js

---
+++ JS/FindTheIndexOfTheFirstOccurrenceInString.js
@@ -0,0 +1,15 @@
+/**
+ * @version JAVASCRIPT ECMAScript 6
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+function strStr(haystack, needle) {
+    return haystack.indexOf(needle);
+}
+export { strStr }
