5dd93756a83202bb65f19b6fda3ff03856020f31
Mario SAM <eu@mariosam.com.br>
2023-03-21 00:13:21 -0300

    Create FindTheIndexOfTheFirstOccurrenceInString.go

---
+++ GO/FindTheIndexOfTheFirstOccurrenceInString.go
@@ -0,0 +1,12 @@
+/**
+ * @version GO 1.20.2
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+import "strings"
+
+func strStr(haystack string, needle string) int {
+	return strings.Index(haystack, needle)
+}
