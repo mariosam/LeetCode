3aebb32ebfeab10a60ec3d87ad5e9931fd2aa5aa
Mario SAM <eu@mariosam.com.br>
2023-04-27 01:24:54 -0300

    Create BulbSwitcher.php

---
+++ PHP/BulbSwitcher.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+class BulbSwitcher {    
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function bulbSwitch($n) {
+        return intval(Sqrt($n));
+    }
+
+}
