e8ea2bc9be27498d734fcb6ae593faa405d0dfc1
Mario SAM <eu@mariosam.com.br>
2023-04-28 02:01:48 -0300

    Create BulbSwitcher.js

---
+++ JS/BulbSwitcher.js
@@ -0,0 +1,14 @@
+/**
+ * @version JAVASCRIPT ECMAScript 6
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function bulbSwitch(n) {
+    return parseInt(Math.sqrt(n));
+}
+export { bulbSwitch }
