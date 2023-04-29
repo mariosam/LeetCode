ab7861460bca2bab973a92caf6905cfc64c01170
Mario SAM <eu@mariosam.com.br>
2023-04-29 00:19:29 -0300

    Create BulbSwitcher.java

---
+++ JAVA/BulbSwitcher.java
@@ -0,0 +1,18 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+public class BulbSwitcher {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", bulbSwitch( 5 ));
+    }
+
+    public static int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+
+}
