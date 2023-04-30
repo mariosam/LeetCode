9f2a3dfea5c28bc5d0c2dab58c2a74b3c6f39af7
Mario SAM <eu@mariosam.com.br>
2023-04-30 00:06:15 -0300

    Create BulbSwitcher.go

---
+++ GO/BulbSwitcher.go
@@ -0,0 +1,12 @@
+/**
+ * @version GO 1.20.3
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+import "math"
+
+func bulbSwitch(n int) int {
+	return int(math.Sqrt(float64(n)))
+}
