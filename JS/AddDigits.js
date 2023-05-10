b59bf3cd7bb9b9dd3cb7321b651f18a57f0fd6c4
Mario SAM <eu@mariosam.com.br>
2023-05-10 00:28:08 -0300

    Create AddDigits.js

---
+++ JS/AddDigits.js
@@ -0,0 +1,14 @@
+/**
+ * @version JAVASCRIPT ECMAScript 6
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+function addDigits(num) {
+    return (num-1)%9 + 1;
+}
+export { addDigits }
