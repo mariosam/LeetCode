29c5ed9d1ddb2cc2da0b22c35a1374a8b8139ed8
Mario SAM <eu@mariosam.com.br>
2023-05-12 00:15:07 -0300

    Create AddDigits.go

---
+++ GO/AddDigits.go
@@ -0,0 +1,10 @@
+/**
+ * @version GO 1.20.3
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+func addDigits(num int) int {
+	return (num-1)%9 + 1
+}
