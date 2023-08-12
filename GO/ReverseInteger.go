ab4c75c9f70c4527299575def30a564ab5ba1b0f
Mario SAM <eu@mariosam.com.br>
2023-08-12 01:49:41 -0300

    Create ReverseInteger.go

---
+++ GO/ReverseInteger.go
@@ -0,0 +1,26 @@
+/**
+ * @version GO 1.20.1
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+import "math"
+
+func reverse(x int) int {
+	var rev int
+
+	for x != 0 {
+		pop := x % 10
+		x /= 10
+		if rev > math.MaxInt32/10 || (rev == math.MaxInt32/10 && pop > 7) {
+			return 0
+		}
+		if rev < math.MinInt32/10 || (rev == math.MinInt32/10 && pop < -8) {
+			return 0
+		}
+		rev = rev*10 + pop
+	}
+
+	return rev
+}
