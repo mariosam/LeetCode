39ce9bd6b0c681ef656e0f8e76774e0425d1fca3
Mario SAM <eu@mariosam.com.br>
2023-08-27 01:14:43 -0300

    Create CombinationsTest.java

---
+++ JAVA/CombinationsTest.java
@@ -0,0 +1,19 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import org.junit.Test;
+
+public class CombinationsTest {
+
+    @Test
+	public void testCombinations() throws Exception {
+        //Test 1
+        
+	}
+
+}
