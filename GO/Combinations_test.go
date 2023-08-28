564dcdea21f50f7a40a9c04aa1ed542ce554a4fe
Mario SAM <eu@mariosam.com.br>
2023-08-28 01:08:10 -0300

    Create Combinations_test.go

---
+++ GO/Combinations_test.go
@@ -0,0 +1,31 @@
+/**
+ * @version GO 1.20.5
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ go test -timeout 999999s -run TestCombinations
+ */
+package GO
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCombinations(t *testing.T) {
+	tables := []struct {
+		want [][]int
+		n    int
+		k    int
+	}{
+		//{{{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}}, 4, 2},
+		//{{{1}}, 1, 1},
+	}
+
+	for _, table := range tables {
+		got := combine(table.n, table.k)
+
+		if !reflect.DeepEqual(got, table.want) {
+			t.Errorf("Waiting for this %d but the return was this: %d", table.want, got)
+		}
+	}
+}
