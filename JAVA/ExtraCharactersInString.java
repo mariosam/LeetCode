08b1aad0c5f9f2431cf5c5f8993ce420cab26944
Mario SAM <eu@mariosam.com.br>
2023-09-12 02:10:00 -0300

    Create ExtraCharactersInString.java

---
+++ JAVA/ExtraCharactersInString.java
@@ -0,0 +1,37 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExtraCharactersInString {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", minExtraChar( "leetscode", new String[]{"leet","code","leetcode"} ));
+    }
+
+    static int res;
+    
+    public static int minExtraChar(String s, String[] dictionary) {
+        Set<String> ss = new HashSet<>();
+        for (String w : dictionary) {
+            ss.add(w);
+        }
+        int n = s.length();
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            f[i] = f[i - 1] + 1;
+            for (int j = 0; j < i; ++j) {
+                if (ss.contains(s.substring(j, i))) {
+                    f[i] = Math.min(f[i], f[j]);
+                }
+            }
+        }
+        return f[n];
+    }
+}
