13da11ce2561bf9a82021d69191734d08df8fcc3
Mario SAM <eu@mariosam.com.br>
2023-09-14 00:10:42 -0300

    Create MaximumLengthOfPairChainTest.php

---
+++ PHP/MaximumLengthOfPairChainTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ ./vendor/bin/phpunit MaximumLengthOfPairChainTest.php
+ */
+namespace PHP;
+
+use PHPUnit\Framework\TestCase;
+require ("MaximumLengthOfPairChain.php");
+
+class MaximumLengthOfPairChainTest extends TestCase {
+
+    public function testMaximumLengthOfPairChain() {
+        $obj = new MaximumLengthOfPairChain();
+        //Test 1
+        $want = 2;
+        $got = $obj->findLongestChain( [[1,2],[2,3],[3,4]] );
+        echo "\nTest 1: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+        //Test 2
+        $want = 3;
+        $got = $obj->findLongestChain( [[1,2],[7,8],[4,5]] );
+        echo "\nTest 2: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+    }
+}
