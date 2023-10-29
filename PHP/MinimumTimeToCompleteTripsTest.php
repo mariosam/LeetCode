eba2453a0d6e574a9b517512f5e97cf1a4046224
Mario SAM <eu@mariosam.com.br>
2023-10-29 00:52:28 -0300

    Create MinimumTimeToCompleteTripsTest.php

---
+++ PHP/MinimumTimeToCompleteTripsTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ ./vendor/bin/phpunit MinimumTimeToCompleteTripsTest.php
+ */
+namespace PHP;
+
+use PHPUnit\Framework\TestCase;
+require ("MinimumTimeToCompleteTrips.php");
+
+class MinimumTimeToCompleteTripsTest extends TestCase {
+
+    public function testMinimumTimeToCompleteTrips() {
+        $obj = new MinimumTimeToCompleteTrips();
+        //Test 1
+        $want = 3;
+        $got = $obj->minimumTime( [1,2,3], 5 );
+        echo "\nTest 1: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+        //Test 2
+        $want = 2;
+        $got = $obj->minimumTime( [2], 1 );
+        echo "\nTest 2: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+    }
+}
