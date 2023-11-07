0ab93a4e63f26000eb05d75b3df0bc6330ba1393
Mario SAM <eu@mariosam.com.br>
2023-11-07 01:39:39 -0300

    Update FindTheDuplicateNumber.php

--- PHP/FindTheDuplicateNumber.php
+++ PHP/FindTheDuplicateNumber.php
@@ -1,4 +1,12 @@
-class Solution {
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+class FindTheDuplicateNumber {    
 
     /**
      * @param Integer[] $nums
@@ -27,4 +35,5 @@ class Solution {
 
         return $left;
     }
+
 }
