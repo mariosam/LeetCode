d96bd78a579e41313a3fc24d35ded1d834bf4916
Mario SAM <eu@mariosam.com.br>
2023-12-01 00:23:57 -0300

    Create MinimumNumberOfOperationsToMakeArrayContinuous_test.go

---
+++ GO/MinimumNumberOfOperationsToMakeArrayContinuous_test.go
@@ -0,0 +1,31 @@
+/**
+ * @version GO 1.21.1
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ go test -timeout 999999s -run TestMinimumNumberOfOperationsToMakeArrayContinuous
+ */
+package GO
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMinimumNumberOfOperationsToMakeArrayContinuous(t *testing.T) {
+	tables := []struct {
+		want int
+		nums []int
+	}{
+		{0, []int{4, 2, 5, 3}},
+		{1, []int{1, 2, 3, 5, 6}},
+		{3, []int{1, 10, 100, 1000}},
+	}
+
+	for _, table := range tables {
+		got := minOperations(table.nums)
+
+		if !reflect.DeepEqual(got, table.want) {
+			t.Errorf("Waiting for this %d but the return was this: %d", table.want, got)
+		}
+	}
+}
