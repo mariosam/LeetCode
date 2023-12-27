dbe7f5ea85ef0115b10900723c03ebd0a956eaf0
Mario SAM <eu@mariosam.com.br>
2023-12-27 01:40:16 -0300

    Create StrangePrinter.java

---
+++ JAVA/StrangePrinter.java
@@ -0,0 +1,35 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+public class StrangePrinter {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", strangePrinter( "aba" ));
+    }
+
+    public static int strangePrinter(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    dp[i][j] = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+
+}
