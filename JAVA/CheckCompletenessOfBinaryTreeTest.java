56e555fb83741bab75619719d3c3538237cea51d
Mario SAM <eu@mariosam.com.br>
2024-01-08 03:31:38 -0300

    Create CheckCompletenessOfBinaryTreeTest.java

---
+++ JAVA/CheckCompletenessOfBinaryTreeTest.java
@@ -0,0 +1,20 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CheckCompletenessOfBinaryTreeTest {
+
+    @Test
+	public void testCheckCompletenessOfBinaryTree() throws Exception {
+        //Test 1
+
+	}
+
+}
