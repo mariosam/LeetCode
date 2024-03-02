93fb0113a5a254d501a8eb323837cf379e43562d
Mario SAM <eu@mariosam.com.br>
2024-03-02 00:14:39 -0300

    Create MaximumOddBinaryNumberTest.php

---
+++ PHP/MaximumOddBinaryNumberTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ ./vendor/bin/phpunit MaximumOddBinaryNumberTest.php
+ */
+namespace PHP;
+
+use PHPUnit\Framework\TestCase;
+require ("MaximumOddBinaryNumber.php");
+
+class MaximumOddBinaryNumberTest extends TestCase {
+
+    public function testMaximumOddBinaryNumber() {
+        $obj = new MaximumOddBinaryNumber();
+        //Test 1
+        $want = "001";
+        $got = $obj->maximumOddBinaryNumber( "010" );
+        echo "\nTest 1: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+        //Test 2
+        $want = "1001";
+        $got = $obj->maximumOddBinaryNumber( "0101" );
+        echo "\nTest 2: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+    }
+}
