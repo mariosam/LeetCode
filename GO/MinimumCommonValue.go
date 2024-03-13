265f121a46ac652114112ab8a31b9f4cdd42a65e
Mario SAM <eu@mariosam.com.br>
2024-03-13 14:50:48 -0300

    Create MinimumCommonValue.go

---
+++ GO/MinimumCommonValue.go
@@ -0,0 +1,21 @@
+/**
+ * @version GO 1.22.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+func getCommon(nums1 []int, nums2 []int) int {
+	m, n := len(nums1), len(nums2)
+	for i, j := 0, 0; i < m && j < n; {
+		if nums1[i] == nums2[j] {
+			return nums1[i]
+		}
+		if nums1[i] < nums2[j] {
+			i++
+		} else {
+			j++
+		}
+	}
+	return -1
+}
