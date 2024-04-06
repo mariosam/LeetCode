3bcd2dfdf23c48e94fc33b1b9c837092501065bb
Mario SAM <eu@mariosam.com.br>
2024-04-06 00:33:50 -0300

    Create WordSearch_test.go

---
+++ GO/WordSearch_test.go
@@ -0,0 +1,32 @@
+/**
+ * @version GO 1.22.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ go test -timeout 999999s -run TestWordSearch
+ */
+package GO
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWordSearch(t *testing.T) {
+	tables := []struct {
+		want  bool
+		board [][]byte
+		strs  string
+	}{
+		{true, [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"},
+		{true, [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "SEE"},
+		{false, [][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCB"},
+	}
+
+	for _, table := range tables {
+		got := exist(table.board, table.strs)
+
+		if !reflect.DeepEqual(got, table.want) {
+			t.Errorf("Waiting for this %t but the return was this: %t", table.want, got)
+		}
+	}
+}
