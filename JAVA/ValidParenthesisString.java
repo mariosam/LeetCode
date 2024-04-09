edbd6509d2a27a070650af43f567a701e5d0ace1
Mario SAM <eu@mariosam.com.br>
2024-04-09 00:03:48 -0300

    Create ValidParenthesisString.java

---
+++ JAVA/ValidParenthesisString.java
@@ -0,0 +1,25 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+public class ValidParenthesisString {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", checkValidString( "121" ));
+    }
+
+    public static boolean checkValidString(String s) {
+        int lo = 0, hi = 0;
+        for (char c: s.toCharArray()) {
+            lo += c == '(' ? 1 : -1;
+            hi += c != ')' ? 1 : -1;
+            if (hi < 0) break;
+            lo = Math.max(lo, 0);
+        }
+        return lo == 0;
+    }
+
+}
