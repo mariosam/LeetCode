5677b123cd1f5dacee1bfe8654ec7cdc0c1991b9
Mario SAM <eu@mariosam.com.br>
2024-04-13 00:11:34 -0300

    Create TimeNeededToBuyTickets.java

---
+++ JAVA/TimeNeededToBuyTickets.java
@@ -0,0 +1,26 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+public class TimeNeededToBuyTickets {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", timeRequiredToBuy(new int[]{2,7,11,11}, 9));
+    }
+
+    public static int timeRequiredToBuy(int[] tickets, int k) {
+        int ans = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                ans += Math.min(tickets[k], tickets[i]);
+            } else {
+                ans += Math.min(tickets[k] - 1, tickets[i]);
+            }
+        }
+        return ans;
+    }
+
+}
