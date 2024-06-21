11838fd7a64476a35ee2474a0fe7e64a8fb9f7d7
Mario SAM <eu@mariosam.com.br>
2024-06-21 00:06:34 -0300

    Create SumOfSquareNumbers.go

---
+++ GO/SumOfSquareNumbers.go
@@ -0,0 +1,24 @@
+/**
+ * @version GO 1.22.3
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+import "math"
+
+func judgeSquareSum(c int) bool {
+	a, b := 0, int(math.Sqrt(float64(c)))
+	for a <= b {
+		s := a*a + b*b
+		if s == c {
+			return true
+		}
+		if s < c {
+			a++
+		} else {
+			b--
+		}
+	}
+	return false
+}
