c2df885381ebdc0bbbd852aadd531c146013d9e9
Mario SAM <eu@mariosam.com.br>
2024-08-19 00:14:39 -0300

    Create LemonadeChange.java

---
+++ JAVA/LemonadeChange.java
@@ -0,0 +1,26 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+public class LemonadeChange {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", lemonadeChange( new int[]{5,5,5,10,20} ));
+    }
+
+    public static boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int i : bills) {
+            if (i == 5) five++;
+            else if (i == 10) {five--; ten++;}
+            else if (ten > 0) {ten--; five--;}
+            else five -= 3;
+            if (five < 0) return false;
+        }
+        return true;
+    }
+
+}
