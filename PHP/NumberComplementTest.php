a41232624d2cefa576eecc840504b3b179f2db29
Mario SAM <eu@mariosam.com.br>
2024-08-25 00:01:57 -0300

    Create NumberComplementTest.php

---
+++ PHP/NumberComplementTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ ./vendor/bin/phpunit NumberComplementTest.php
+ */
+namespace PHP;
+
+use PHPUnit\Framework\TestCase;
+require ("NumberComplement.php");
+
+class NumberComplementTest extends TestCase {
+
+    public function testNumberComplement() {
+        $obj = new NumberComplement();
+        //Test 1
+        $want = 2;
+        $got = $obj->findComplement( 5 );
+        echo "\nTest 1: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+        //Test 2
+        $want = 0;
+        $got = $obj->findComplement( 1 );
+        echo "\nTest 2: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+    }
+}
