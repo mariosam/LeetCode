396a09c0794ce9c293463d48b2b5439d725508dd
Mario SAM <eu@mariosam.com.br>
2024-08-30 00:13:49 -0300

    Create CountSubIslands.test.js

---
+++ JS/CountSubIslands.test.js
@@ -0,0 +1,21 @@
+/**
+ * @version JAVASCRIPT ECMAScript 6 
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ node NomeDaClasse.test
+ */
+import TEST from 'tape'
+import { countSubIslands } from './CountSubIslands.js'
+
+TEST('Starting CountSubIslands test...', (t) => {
+    //Test 1
+    let want = 3
+    let got = countSubIslands( [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]] )
+    t.assert( want === got, "Expect: "+want)
+    //Test 2
+    want = 2
+    got = countSubIslands( [[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]] )
+    t.assert( want === got, "Expect: "+want)
+
+    t.end()
+})
