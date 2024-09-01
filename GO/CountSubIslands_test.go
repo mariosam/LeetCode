924e8493c838da9f3f471374e4449bccbf5a02a0
Mario SAM <eu@mariosam.com.br>
2024-09-01 00:04:36 -0300

    Create CountSubIslands_test.go

---
+++ GO/CountSubIslands_test.go
@@ -0,0 +1,31 @@
+/**
+ * @version GO 1.22.3
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ go test -timeout 999999s -run TestCountSubIslands
+ */
+package GO
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCountSubIslands(t *testing.T) {
+	tables := []struct {
+		want  int
+		word1 [][]int
+		word2 [][]int
+	}{
+		{3, [][]int{{1, 1, 1, 0, 0}, {0, 1, 1, 1, 1}, {0, 0, 0, 0, 0}, {1, 0, 0, 0, 0}, {1, 1, 0, 1, 1}}, [][]int{{1, 1, 1, 0, 0}, {0, 0, 1, 1, 1}, {0, 1, 0, 0, 0}, {1, 0, 1, 1, 0}, {0, 1, 0, 1, 0}}},
+		{2, [][]int{{1, 0, 1, 0, 1}, {1, 1, 1, 1, 1}, {0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {1, 0, 1, 0, 1}}, [][]int{{0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {0, 1, 0, 1, 0}, {0, 1, 0, 1, 0}, {1, 0, 0, 0, 1}}},
+	}
+
+	for _, table := range tables {
+		got := countSubIslands(table.word1, table.word2)
+
+		if !reflect.DeepEqual(got, table.want) {
+			t.Errorf("Waiting for this %d but the return was this: %d", table.want, got)
+		}
+	}
+}
