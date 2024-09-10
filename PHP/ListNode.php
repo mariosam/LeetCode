a72c963f4fd2e517ea52f15509529c08c4e391b5
Mario SAM <eu@mariosam.com.br>
2024-09-10 15:02:30 -0300

    Create ListNode.php

---
+++ PHP/ListNode.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+class ListNode {
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val = 0, $next = null) {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
