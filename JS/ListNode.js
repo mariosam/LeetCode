cb67b7a0517e56f35bfd83030594753d2233ae44
Mario SAM <eu@mariosam.com.br>
2024-09-11 00:14:38 -0300

    Create ListNode.js

---
+++ JS/ListNode.js
@@ -0,0 +1,13 @@
+/**
+ * @version JAVASCRIPT ECMAScript 6
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+
+// ListNode.mjs
+export default class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
