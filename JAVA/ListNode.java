a1206abb21f2b5458363df6c7edd3c65115493f5
Mario SAM <eu@mariosam.com.br>
2024-09-12 00:15:13 -0300

    Create ListNode.java

---
+++ JAVA/ListNode.java
@@ -0,0 +1,14 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
