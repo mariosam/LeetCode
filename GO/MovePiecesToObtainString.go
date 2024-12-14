e9f383e62cab07512a102f6fead7c788a9d7e15d
Mario SAM <eu@mariosam.com.br>
2024-12-14 00:16:16 -0300

    Create MovePiecesToObtainString.go

---
+++ GO/MovePiecesToObtainString.go
@@ -0,0 +1,32 @@
+/**
+ * @version GO 1.22.3
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+func canChange(start string, target string) bool {
+	n := len(start)
+	i, j := 0, 0
+	for {
+		for i < n && start[i] == '_' {
+			i++
+		}
+		for j < n && target[j] == '_' {
+			j++
+		}
+		if i == n && j == n {
+			return true
+		}
+		if i == n || j == n || start[i] != target[j] {
+			return false
+		}
+		if start[i] == 'L' && i < j {
+			return false
+		}
+		if start[i] == 'R' && i > j {
+			return false
+		}
+		i, j = i+1, j+1
+	}
+}
