b1bf472d831648c8f04548c55d5e9095bb8d49f2
Mario SAM <eu@mariosam.com.br>
2025-02-12 00:06:44 -0300

    Create CountNumberOfBadPairs_test.go

---
+++ GO/CountNumberOfBadPairs_test.go
@@ -0,0 +1,30 @@
+/**
+ * @version GO 1.23.5
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ go test -timeout 999999s -run TestCountNumberOfBadPairs
+ */
+package GO
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCountNumberOfBadPairs(t *testing.T) {
+	tables := []struct {
+		want int64
+		low  []int
+	}{
+		{5, []int{4, 1, 3, 3}},
+		{0, []int{1, 2, 3, 4, 5}},
+	}
+
+	for _, table := range tables {
+		got := countBadPairs(table.low)
+
+		if !reflect.DeepEqual(got, table.want) {
+			t.Errorf("Waiting for this %d but the return was this: %d", table.want, got)
+		}
+	}
+}
