722d589d3f36e1cc3ba5ee61eb919e96d04874dd
Mario SAM <eu@mariosam.com.br>
2025-02-24 00:07:50 -0300

    Create RemoveAllOccurrencesOfaSubstring.go

---
+++ GO/RemoveAllOccurrencesOfaSubstring.go
@@ -0,0 +1,15 @@
+/**
+ * @version GO 1.23.5
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+import "strings"
+
+func removeOccurrences(s string, part string) string {
+	for strings.Contains(s, part) {
+		s = strings.Replace(s, part, "", 1)
+	}
+	return s
+}
