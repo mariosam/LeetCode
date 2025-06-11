11102f1bfcb81044f3afab914e83f416f96a4dd8
Mario SAM <eu@mariosam.com.br>
2025-06-11 00:02:40 -0300

    Create LexicographicalNumbers.java

---
+++ JAVA/LexicographicalNumbers.java
@@ -0,0 +1,38 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LexicographicalNumbers {
+
+    private static final int[] p = new int[26];
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", lexicalOrder(8));
+    }
+
+    public static List<Integer> lexicalOrder(int n) {
+        List<Integer> list = new ArrayList<>(n);
+        int curr = 1;
+        for (int i = 1; i <= n; i++) {
+            list.add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
+            } else if (curr % 10 != 9 && curr + 1 <= n) {
+                curr++;
+            } else {
+                while ((curr / 10) % 10 == 9) {
+                    curr /= 10;
+                }
+                curr = curr / 10 + 1;
+            }
+        }
+        return list;
+    }
+
+}
