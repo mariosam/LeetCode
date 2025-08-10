472f7ac90d0dcf46303acff90c8e77c33f1e880c
Mario SAM <eu@mariosam.com.br>
2025-08-10 00:29:25 -0300

    Create FruitIntoBasketsTest.java

---
+++ JAVA/FruitIntoBasketsTest.java
@@ -0,0 +1,30 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class FruitIntoBasketsTest {
+
+    @Test
+	public void testFruitIntoBaskets() throws Exception {
+        //Test 1
+        int want = 3;
+        int got = FruitIntoBaskets.totalFruit( new int[]{1,2,1} );
+        assertEquals(want, got);
+        //Test 2
+        want = 3;
+        got = FruitIntoBaskets.totalFruit( new int[]{0,1,2,2} );
+        assertEquals(want, got);
+        //Test 3
+        want = 4;
+        got = FruitIntoBaskets.totalFruit( new int[]{1,2,3,2,2} );
+        assertEquals(want, got);
+	}
+
+}
