93ebed4766f646172c77397540cecd43f59091f5
Mario SAM <eu@mariosam.com.br>
2025-08-26 00:27:13 -0300

    Create LongestSubarrayOf1sAfterDeletingOneElement.java

---
+++ JAVA/LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -0,0 +1,39 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+public class LongestSubarrayOf1sAfterDeletingOneElement {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", longestSubarray( new int[]{1,2,3} ));
+    }
+
+    public static int longestSubarray(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] == 1) {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i + 1] == 1) {
+                right[i] = right[i + 1] + 1;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, left[i] + right[i]);
+        }
+
+        return ans;
+    }
+
+}
