df6d9daf9a79317f75a0946f351af73dddafce37
Mario SAM <eu@mariosam.com.br>
2025-08-26 00:26:43 -0300

    Create LongestSubarrayOf1sAfterDeletingOneElementTest.java

---
+++ JAVA/LongestSubarrayOf1sAfterDeletingOneElementTest.java
@@ -0,0 +1,30 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class LongestSubarrayOf1sAfterDeletingOneElementTest {
+
+    @Test
+	public void testLongestSubarrayOf1sAfterDeletingOneElement() throws Exception {
+        //Test 1
+        int want = 3;
+        int got = LongestSubarrayOf1sAfterDeletingOneElement.longestSubarray( new int[]{1,1,0,1} );
+        assertEquals(want, got);
+        //Test 2
+        want = 5;
+        got = LongestSubarrayOf1sAfterDeletingOneElement.longestSubarray( new int[]{0,1,1,1,0,1,1,0,1} );
+        assertEquals(want, got);
+        //Test 3
+        want = 2;
+        got = LongestSubarrayOf1sAfterDeletingOneElement.longestSubarray( new int[]{1,1,1} );
+        assertEquals(want, got);
+	}
+
+}
