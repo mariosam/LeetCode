de2d7500c32d61fe8fb9515a8747ac91fb973a21
Mario SAM <eu@mariosam.com.br>
2025-12-01 00:15:09 -0300

    Add test for smallest integer divisible by K

---
+++ GO/SmallestIntegerDivisibleByK_test.go
@@ -0,0 +1,31 @@
+/**
+ * @version GO 1.23.5
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ go test -timeout 999999s -run TestSmallestIntegerDivisibleByK
+ */
+package GO
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSmallestIntegerDivisibleByK(t *testing.T) {
+	tables := []struct {
+		want int
+		nums int
+	}{
+		{1, 1},
+		{-1, 2},
+		{3, 3},
+	}
+
+	for _, table := range tables {
+		got := smallestRepunitDivByK(table.nums)
+
+		if !reflect.DeepEqual(got, table.want) {
+			t.Errorf("Waiting for this %d but the return was this: %d", table.want, got)
+		}
+	}
+}
