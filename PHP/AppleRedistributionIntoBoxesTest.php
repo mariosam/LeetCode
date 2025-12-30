b410a71dc64beeaec6b52cf84e5756fbbd8e6916
Mario SAM <eu@mariosam.com.br>
2025-12-30 00:05:07 -0300

    Add unit tests for AppleRedistributionIntoBoxes

---
+++ PHP/AppleRedistributionIntoBoxesTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ ./vendor/bin/phpunit AppleRedistributionIntoBoxesTest.php
+ */
+namespace PHP;
+
+use PHPUnit\Framework\TestCase;
+require ("AppleRedistributionIntoBoxes.php");
+
+class AppleRedistributionIntoBoxesTest extends TestCase {
+
+    public function testAppleRedistributionIntoBoxes() {
+        $obj = new AppleRedistributionIntoBoxes();
+        //Test 1
+        $want = 2;
+        $got = $obj->minimumBoxes( [1,3,2], [4,3,1,5,2] );
+        echo "\nTest 1: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+        //Test 2
+        $want = 4;
+        $got = $obj->minimumBoxes( [5,5,5], [2,4,2,7] );
+        echo "\nTest 2: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+    }
+}
