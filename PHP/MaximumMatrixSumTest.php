f610b17aadd593d873213c4d4d5a2bcb2d926779
Mario SAM <eu@mariosam.com.br>
2026-01-07 00:03:51 -0300

    Create MaximumMatrixSumTest.php

---
+++ PHP/MaximumMatrixSumTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version PHP 8.2.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ ./vendor/bin/phpunit MaximumMatrixSumTest.php
+ */
+namespace PHP;
+
+use PHPUnit\Framework\TestCase;
+require ("MaximumMatrixSum.php");
+
+class MaximumMatrixSumTest extends TestCase {
+
+    public function testMaximumMatrixSum() {
+        $obj = new MaximumMatrixSum();
+        //Test 1
+        $want = 4;
+        $got = $obj->maxMatrixSum( [[1,-1],[-1,1]] );
+        echo "\nTest 1: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+        //Test 2
+        $want = 16;
+        $got = $obj->maxMatrixSum( [[1,2,3],[-1,-2,-3],[1,2,3]] );
+        echo "\nTest 2: retornou " . $got . " == esperado: " . $want;
+        $this->assertEquals($want, $got);
+    }
+}
