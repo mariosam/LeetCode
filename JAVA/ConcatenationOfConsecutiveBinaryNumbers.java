fea9f292a29cd4b89d34dda64c22ef0f07287789
Mario SAM <eu@mariosam.com.br>
2026-03-02 00:18:12 -0300

    Add ConcatenationOfConsecutiveBinaryNumbers class

---
+++ JAVA/ConcatenationOfConsecutiveBinaryNumbers.java
@@ -0,0 +1,25 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package JAVA;
+
+public class ConcatenationOfConsecutiveBinaryNumbers {
+
+    public static void main(String[] args) {
+        System.out.printf("Resultado: %d\n", concatenatedBinary(3));
+    }
+
+    public static int concatenatedBinary(int n) {
+        final int MOD = 1000000007;
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            String binary = Integer.toBinaryString(i);
+            for (int j = 0; j < binary.length(); j++) {
+                result = (result * 2 + (binary.charAt(j) - '0')) % MOD;
+            }
+        }
+        return result;
+    }
+}
