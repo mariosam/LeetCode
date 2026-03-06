7ac853c9c8ecc8d89c028c3cd4fc150603d430e5
Mario SAM <eu@mariosam.com.br>
2026-03-06 00:17:41 -0300

    Create SpecialPositionsInBinaryMatrixTest.java

---
+++ JAVA/SpecialPositionsInBinaryMatrixTest.java
@@ -0,0 +1,27 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SpecialPositionsInBinaryMatrixTest {
+
+    @Test
+	public void testSpecialPositionsInBinaryMatrix() throws Exception {
+        //Test 1
+        int want = 1;
+        int got = SpecialPositionsInBinaryMatrix.numSpecial(new int[][] {{1, 0, 0}, {0, 0, 1}, {1, 0, 0}});
+        assertEquals(want, got);
+        //Test 2
+        want = 3;
+        got = SpecialPositionsInBinaryMatrix.numSpecial(new int[][] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
+        assertEquals(want, got);
+	}
+
+}
