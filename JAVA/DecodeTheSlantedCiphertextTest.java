8cfd0df81395893118c43d74dda54cef8e0b45b7
Mario SAM <eu@mariosam.com.br>
2026-04-11 00:12:37 -0300

    Create DecodeTheSlantedCiphertextTest.java

---
+++ JAVA/DecodeTheSlantedCiphertextTest.java
@@ -0,0 +1,31 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DecodeTheSlantedCiphertextTest {
+
+    @Test
+	public void testDecodeTheSlantedCiphertext() throws Exception {
+        //Test 1
+        String want = "cipher";
+        String got = DecodeTheSlantedCiphertext.decodeCiphertext("ch   ie   pr", 3);
+        assertEquals(want, got);
+        //Test 2
+        want = "i love leetcode";
+        got = DecodeTheSlantedCiphertext.decodeCiphertext("iveo    eed   l te   olc", 4);
+        assertEquals(want, got);
+        //Test 3
+        want = "coding";
+        got = DecodeTheSlantedCiphertext.decodeCiphertext("coding", 1);
+        assertEquals(want, got);
+	}
+
+}
