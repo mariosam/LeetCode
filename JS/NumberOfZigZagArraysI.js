4cbcd0d27d21ea72174785414d15aaf34a65a4d4
Mario SAM <eu@mariosam.com.br>
2026-06-25 00:10:46 -0300

    Add zigZagArrays function for calculating arrays

---
+++ JS/NumberOfZigZagArraysI.js
@@ -0,0 +1,35 @@
+/**
+ * @version JAVASCRIPT ECMAScript 6
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+
+/**
+ * @param {number} n
+ * @param {number} l
+ * @param {number} r
+ * @return {number}
+ */
+function zigZagArrays(n, l, r) {
+    r -= l;
+    let dp = new Array(r + 1).fill(1);
+    const mod = 10 ** 9 + 7;
+    for (let i = 1; i < n; i++) {
+        let pre = 0;
+        if (i & 1) {
+            for (let v = 0; v <= r; v++) {
+                let pre2 = pre + dp[v];
+                dp[v] = pre;
+                pre = pre2 % mod;
+            }
+        } else {
+            for (let v = r; v >= 0; v--) {
+                let pre2 = pre + dp[v];
+                dp[v] = pre;
+                pre = pre2 % mod;
+            }
+        }
+    }
+    return (dp.reduce((a, b) => a + b, 0) * 2) % mod;
+}
+export { zigZagArrays }
