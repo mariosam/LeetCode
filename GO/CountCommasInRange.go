0920f7a2137d4655faf7d60b4fae155cf72f14a5
Mario SAM <eu@mariosam.com.br>
2026-09-12 00:51:26 -0300

    Create CountCommasInRange.go

---
+++ GO/CountCommasInRange.go
@@ -0,0 +1,14 @@
+/**
+ * @version GO 1.26.0
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+func countCommas(n int) int {
+	if n >= 1000 {
+
+		return n - 999
+	}
+	return 0
+}
